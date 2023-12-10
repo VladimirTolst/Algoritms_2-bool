b26f32e350d146b81249dc90b2373ddf7d5cd385
Legebd <Legebd@DESKTOP-M1BP9AF>
2023-12-10 21:14:14 +0700

    Добавьте файлы проекта.

---
+++ Algoritms_2(bool)/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Timers;
+
+bool[,] inputBool =
+    {{false,false,false},
+    {false,false,true}, 
+    {false,true,false}, 
+    {false,true,true},
+    {true,false,false},
+    {true,false,true},
+    {true,true,false},
+    {true,true,true}};
+
+Console.WriteLine("x y z !x !y !x|!y !z !x|!y|!z x|!y&!z");
+for (int i = 0; i < 8; i++)
+{
+    Console.Write(Convert.ToInt32(inputBool[i, 0]) + " ");
+    Console.Write(Convert.ToInt32(inputBool[i, 1]) + " ");
+    Console.Write(Convert.ToInt32(inputBool[i, 2]) + " ");
+    if (!inputBool[i, 0])
+    {
+        Console.Write(" 1 ");
+    }
+    else
+    {
+        Console.Write(" 0 ");
+    }
+
+    if (!inputBool[i, 1]) 
+    {
+        Console.Write(" 1 ");
+    }
+    else
+    {
+        Console.Write(" 0 ");
+    }
+
+    if (!inputBool[i, 0] || !inputBool[i, 1])
+    {
+        Console.Write("  1   ");
+    }
+    else
+    {
+        Console.Write("  0   ");
+    }
+
+    if (inputBool[i, 2])
+    {
+        Console.Write(" 1 ");
+    }
+    else
+    {
+        Console.Write(" 0 ");
+    }
+
+    if (!inputBool[i, 0] || !inputBool[i, 1] || !inputBool[i,2])
+    {
+        Console.Write("   1    ");
+    }
+    else
+    {
+        Console.Write("   0    ");
+    }
+
+    if (inputBool[i, 0] || !inputBool[i, 1] & !inputBool[i,2])
+    {
+        Console.Write("   1    ");
+    }
+    else
+    {
+        Console.Write("   0    ");
+    }
+    Console.WriteLine();
+
+
+
+}
